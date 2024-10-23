bfcd40dee18dcc5bc731c1c6f41403c9ba2478f5
Henrique Lima (Jappa) <jappadev@gmail.com>
2024-10-23 13:53:20 -0300

    atualizando funções

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from jappalib import interface
+import jappalib.interface
 from time import sleep
+
 
 print('AVISO! Script em construção...')
 sleep(2)
 
-rep = interface.menu(['Equatorial','Saneago', 'Áreas  Técnicas', 'Ultragás', 'Reservatórios', 'VRPs', 'Lixeiras', 'Extintores', 'Hidrantes', 'Check list', 'Manuais', 'Configurações'])
+rep = jappalib.interface.menu(['Equatorial','Saneago', 'Áreas  Técnicas', 'Ultragás', 'Reservatórios', 'VRPs', 'Lixeiras', 'Extintores', 'Hidrantes', 'Check list', 'Manuais', 'Configurações'])
 

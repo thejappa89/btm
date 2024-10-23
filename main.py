d9870863c4a651531f757128393c9913895b8ff5
Henrique Lima (Jappa) <jappadev@gmail.com>
2024-10-23 13:50:12 -0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from btm.jappalib import interface
-from jappalib import *
+from jappalib import interface
 from time import sleep
 
 print('AVISO! Script em construção...')
 sleep(2)
 
-rep = interface.menu(['Equatorial','Saneago', 'Laje Técnica', 'Ultragás', 'Reservatórios', 'VRPs', 'Lixeiras', 'Extintores', 'Hidrantes', 'Check list', 'Manuais', 'Configurações'])
+rep = interface.menu(['Equatorial','Saneago', 'Áreas  Técnicas', 'Ultragás', 'Reservatórios', 'VRPs', 'Lixeiras', 'Extintores', 'Hidrantes', 'Check list', 'Manuais', 'Configurações'])
 

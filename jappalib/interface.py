4fb4f0f6a6507dfb061d5fc73b86446eb263038e
Henrique Lima (Jappa) <jappadev@gmail.com>
2024-10-23 13:39:23 -0300

    Correção de bugs no script

--- jappalib/interface.py
+++ jappalib/interface.py
@@ -1,4 +1,16 @@
-from myfuncs import leiaint
+from jappalib.myfuncs import leiaint
+
+def leiaint(caracteres):
+    while True:
+        try:
+            num = int(input(caracteres))
+        except (ValueError, TypeError):
+            print('ERRO: Por favor, digite um número inteiro válido')
+        except KeyboardInterrupt:
+            print('ERRO: Entrada interrompida pelo usuário.')
+            return 0
+        else:
+            return num
 
 
 def linha(tam=42, caracter='-'):
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from jappalib import interface
+from time import sleep
 
+print('AVISO! Script em construção...')
+sleep(2)
